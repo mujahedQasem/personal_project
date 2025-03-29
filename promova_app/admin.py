866ebe8d6e9a9b4f08c1cd1013a3629539fdb98a
mujahedQasem <mujahed.shiekhqasem@gmail.com>
2025-03-29 04:24:08 +0300

    customize the admin pannel

--- promova_app/admin.py
+++ promova_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from promova_app.models import *
+
+class UserAdmin(admin.ModelAdmin):
+    list_filter = ['created_at']
+    
+
+class MessageAdmin(admin.ModelAdmin):
+    list_filter = ['user']
+
+admin.site.register(Users,UserAdmin)
+
+admin.site.register(MessagesFromUser,MessageAdmin)
+
 # Register your models here.
